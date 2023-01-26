e8ca79fed47f0ed0b0062f3358566e9231799982
ming-napat <nchimsumran1@nait.ca>
2023-01-26 11:48:08 -0700

    Add CanadianIncomeTax stuff

---
+++ NhlSolution/NhlSystemClassLibrary/CanadianIncomeTaxManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystemClassLibrary
+{
+    public class CanadianIncomeTaxManager
+    {
+        public List<CanadianIncomeTaxData> ConvertToCanadianIncomeTax(List<string> lines)
+        {
+            List<CanadianIncomeTaxData> dataList = new List<CanadianIncomeTaxData>();
+            //iterate through each element in the list
+            foreach (string line in lines)
+            {
+                string[] dataFromFile = line.Split(',');
+                CanadianIncomeTaxData data = new CanadianIncomeTaxData();
+                data.RegionAbbreviation = dataFromFile[0];
+                data.RegionName = dataFromFile[1];
+                data.TaxYear = int.Parse(dataFromFile[2]);
+                data.StartingIncome = decimal.Parse(dataFromFile[4]);
+                data.EndingIncome = decimal.Parse(dataFromFile[5]);
+                data.TaxRate = double.Parse(dataFromFile[6]);
+                data.BaseTaxAmount = decimal.Parse(dataFromFile[7]);
+            }
+            return dataList;
+        }
+        public List<string> LoadFromCsvFile(string csvFilePath)
+        {
+            List<string> allLines = new List<string>();
+
+            //use the StreamReader or File class to read data from file
+            //using (StreamReader reader = new StreamReader(csvFilePath))
+            //{
+            //    string line;
+            //    bool firstLine = true;
+            //    while ((line = reader.ReadLine()) != null)
+            //    {
+            //        if (firstLine)
+            //        {
+            //            firstLine = false;
+            //        }
+            //        else
+            //        {
+            //            allLines.Add(line);
+            //        }
+            //    }
+            //}
+            //another way
+            string[] lineArray = File.ReadAllLines(csvFilePath);
+            allLines = lineArray.ToList().Skip(1).ToList();
+
+            return allLines;
+        }
+    }
+}
---
+++ NhlSolution/NhlSystemTestProject/IncomeTaxManagerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystemTestProject
+{
+    [TestClass]
+    public class IncomeTaxManagerTest
+    {
+        [TestMethod]
+        [DataRow(@"..\..\..\data\CanadianPersonalIncomeTaxRates.csv", 439)]
+        public void LoadFromCsvFile_RowCount_CorrectCount(string csvFilePath, int expectedCount)
+        {
+            //Arrange an object to test
+            var dataManager = new CanadianIncomeTaxManager();
+            //Act - perform some task with the object
+            List<string> allLinesFromFile = dataManager.LoadFromCsvFile(csvFilePath);
+            //Assert - call an Assert method
+            //verify that there are 440 records
+            Assert.AreEqual(expectedCount, allLinesFromFile.Count);
+        }
+
+        [TestMethod]
+        [DataRow(@"..\..\..\data\CanadianPersonalIncomeTaxRates.csv", 439)]
+        public void ConvertToCanadianIncomeTax_CorrectConversion_ExpectedResults(string csvFilePath, int expectedCount)
+        {
+            var dataManager = new CanadianIncomeTaxManager();
+            List<string> allLinesFromFile = dataManager.LoadFromCsvFile(csvFilePath);
+            List<CanadianIncomeTaxData> incomeTaxData = dataManager.ConvertToCanadianIncomeTax(allLinesFromFile);
+
+            Assert.AreEqual(expectedCount, allLinesFromFile.Count);
+            //check the contect of the first element and the last element
+            int firstIndex = 0;
+            int lastIndex = incomeTaxData.Count - 1;
+            Assert.AreEqual("CAN", incomeTaxData[firstIndex].RegionAbbreviation);
+            Assert.AreEqual(2023, incomeTaxData[firstIndex].TaxYear);
+            Assert.AreEqual("NU", incomeTaxData[lastIndex].RegionAbbreviation);
+            Assert.AreEqual(2023, incomeTaxData[lastIndex].TaxYear);
+        }
+    }
+}
